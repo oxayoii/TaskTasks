58f65c7023959d4ec954da6d8f9b0164604bf82e
Katya <bykovavkate@mail.ru>
2025-12-12 15:11:22 +0300

    addTwoTasks

---
+++ Task1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int c = 1;
+            StringBuilder res = new StringBuilder();
+            char prevChar = s[0];
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (s[i] == prevChar)
+                {
+                    c++;
+                }
+                else
+                {
+                    if (c > 1)
+                    {
+                        res.Append(prevChar);
+                        res.Append(c);
+                    }
+                    else
+                    {
+                        res.Append(prevChar);
+                    }
+                    prevChar = s[i];
+                    c = 1;
+                }
+            }
+            if (c > 1)
+            {
+                res.Append(prevChar);
+                res.Append(c);
+            }
+            else
+            {
+                res.Append(prevChar);
+            }
+
+            Console.WriteLine("Result: " + res.ToString());
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Task2
+{
+    class Program
+    {
+        static class Server
+        {
+            private static int count = 0;
+            private static readonly ReaderWriterLockSlim _lock = new ReaderWriterLockSlim();
+            public static async Task<int> GetCount()
+            {
+                return await Task.Run(() =>
+                {
+                    _lock.EnterReadLock();
+                    try
+                    {
+                        return count;
+                    }
+                    finally
+                    {
+                        _lock.ExitReadLock();
+                    }
+                });
+            }
+            public static async Task AddToCount(int value)
+            {
+                await Task.Run(() =>
+                {
+                    _lock.EnterWriteLock();
+                    try
+                    {
+                        count += value;
+                    }
+                    finally
+                    {
+                        _lock.ExitWriteLock();
+                    }
+                });
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+          
+        }
+    }
+}
